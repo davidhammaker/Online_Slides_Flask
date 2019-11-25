dd41746a790a7cc0073ccb3d9adc8ef46bee5218
David J. Hammaker <davidjosh93@gmail.com>
2019-11-24 23:54:08 -0500

    Complete proof-of-concept

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = os.environ.get('SECRET')
 socketio = SocketIO(app)
 
@@ -22,10 +24,17 @@ def viewer(id):
     return render_template('viewer.html', id=id)
 
 
-@socketio.on('my event')
-def test_message(message):
+@socketio.on('connection')
+def connection(data):
+    print(data)
     emit('my response', {'data': 'got it!'})
-    print(message)
+
+
+@socketio.on('slide')
+def slide(data):
+    print(data)
+    emit('slide_change', data, broadcast=True)
+    print('sent slide change')
 
 
 if __name__ == '__main__':
